8785ad69e1cee9c70bb77b97951fca779305c32a
s-k-22 <swar.khatale@gmail.com>
2024-04-25 07:40:35 +0530

    bcrypt is added to decode password

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,28 +1,35 @@
 const User = require("../models/users");
+const bcrypt = require("bcrypt");
 
 exports.signup = async (req, res) => {
   const { username, email, password } = req.body;
 
-  const data = await User.create({
-    name: username,
-    email: email,
-    password: password,
-  });
+  bcrypt.hash(password, 5, async (err, hash) => {
+    const data = await User.create({
+      name: username,
+      email,
+      password: hash,
+    });
 
-  res.status(201).json({ userDetails: data });
+    res.status(201).json({ userDetails: data });
+  });
 };
 
 exports.login = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ where: { email: email } });
+
   if (!user) {
     res.status(401).json({ message: "User not found" });
   } else {
-    console.log("found");
-    if (user.password !== password) {
-      res.status(401).json({ message: "User not authorized" });
-    } else {
-      res.status(201).json({ message: "User logged in successfully" });
-    }
+    bcrypt.compare(password, user.password, async (err, result) => {
+      if (err) throw new Error(err);
+      if (result) {
+        res.status(201).json({ message: "User logged in successfully" });
+      } else {
+        res.status(401).json({ message: "User not authorized" });
+      }
+    });
   }
 };
